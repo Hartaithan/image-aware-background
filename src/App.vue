21f337b87496d64bbbdf521793b04ce39f07bad3
hartaithan <hartaithan@gmail.com>
2022-10-15 17:25:08 +0900

    get url from file

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const file = ref<File | null>(null);
+const imgSrc = ref<string | undefined>(undefined)
+
+const onFileChanged = ($event: Event) => {
+  const target = $event.target as HTMLInputElement;
+  if (target && target.files) {
+    const image = target.files[0]
+    file.value = image;
+    imgSrc.value = URL.createObjectURL(image)
+  }
+}
 </script>
 
 <template>
-  Hello world!
+  <input type="file" @change="onFileChanged($event)" accept="image/*" />
+  <img :src="imgSrc" />
 </template>
 
 <style scoped>
