aff6fa11180e808d49dc30efd5a6a9a57092e126
hartaithan <hartaithan@gmail.com>
2022-11-06 16:21:39 +0900

    add adaptive for most used component

--- src/components/MostUsed.vue
+++ src/components/MostUsed.vue
@@ -14,7 +14,8 @@ const props = defineProps<IMostUsedProps>();
       <div class="colors-wrapper">
         <p class="title">MOST USED COLORS</p>
         <div class="colors">
-          <div class="color" v-for="color in props.colors" :key="color.hex" :style="{ background: color.hex }">
+          <div class="color" v-for="(color, index) in props.colors" :id="'color' + (index + 1)" :key="color.hex"
+            :style="{ background: color.hex }">
             <p>RGB: {{ color.rgb }}</p>
             <p>HEX: {{ color.hex }}</p>
           </div>
@@ -32,7 +33,10 @@ const props = defineProps<IMostUsedProps>();
 }
 
 .colors {
-  display: flex;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
   height: 70px;
 }
 
@@ -65,5 +69,48 @@ p {
   font-weight: 600;
   text-align: center;
   margin-bottom: 12px;
+}
+
+@media (max-width: 992px) {
+  .color {
+    height: 60px;
+  }
+
+  .colors {
+    height: auto;
+    min-height: 70px;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+  }
+
+  #color1 {
+    grid-area: 1 / 1 / 2 / 2;
+  }
+
+  #color2 {
+    grid-area: 1 / 2 / 2 / 3;
+  }
+
+  #color3 {
+    grid-area: 2 / 1 / 3 / 2;
+  }
+
+  #color4 {
+    grid-area: 2 / 2 / 3 / 3;
+  }
+
+  #color5 {
+    grid-area: 3 / 1 / 4 / 3;
+  }
+}
+
+@media (max-width: 768px) {
+  .color {
+    height: 40px;
+  }
+
+  .color>p {
+    font-size: 10px;
+  }
 }
 </style>
